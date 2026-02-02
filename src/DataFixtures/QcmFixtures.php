c48f1f8ecab785fc00a6264db40b6497a7b53ad5
Leo Garcia <leo.garcia.5@etu.univ-amu.fr>
2026-02-02 16:48:46 +0100

    Add QcmFixtures

--- src/DataFixtures/QcmFixtures.php
+++ src/DataFixtures/QcmFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Qcm;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,8 +10,36 @@ class QcmFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $qcmsData = [
+            [
+                'title' => 'PHP - Bases',
+                'description' => 'Questions sur les bases du langage PHP',
+            ],
+            [
+                'title' => 'Symfony',
+                'description' => 'QCM sur le framework Symfony',
+            ],
+            [
+                'title' => 'HTML / CSS',
+                'description' => 'Notions essentielles du web',
+            ],
+            [
+                'title' => 'JavaScript',
+                'description' => 'QCM JavaScript niveau débutant',
+            ],
+            [
+                'title' => 'SQL',
+                'description' => 'Requêtes SQL et bases de données',
+            ],
+        ];
+
+        foreach ($qcmsData as $data) {
+            $qcm = new Qcm();
+            $qcm->setTitle($data['title']);
+            $qcm->setDescription($data['description']);
+
+            $manager->persist($qcm);
+        }
 
         $manager->flush();
     }
