30651e78fcc5731197ebba28e4f3a2882821d503
TAMINE-CYRIL <taminecyril@gmail.com>
2026-02-10 11:15:16 +0100

    fix: Refonte du login/register avec une meilleure interface graphique.

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -6,6 +6,7 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -16,74 +17,93 @@ use Symfony\Component\Validator\Constraints\NotBlank;
 
 class RegistrationFormType extends AbstractType
 {
-    /**
-     * Construit le formulaire d'inscription.
-     *
-     * Définit les champs du formulaire :
-     * - email
-     * - acceptation des conditions d'utilisation
-     * - mot de passe (non mappé, encodé dans le contrôleur)
-     *
-     * @param FormBuilderInterface $builder le constructeur de formulaire
-     * @param array $options les options du formulaire
-     */
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('name', TextType::class, [
-                'label' => 'name',
-            ])
-            ->add('surname', TextType::class, [
-                'label' => 'surname',
-            ])
+
             ->add('userType', ChoiceType::class, [
                 'mapped' => false,
                 'expanded' => true,
                 'multiple' => false,
                 'required' => true,
-                'label' => 'Vous êtes :',
+                'label' => 'Vous êtes',
                 'choices' => [
                     'Étudiant' => 'student',
                     'Professeur' => 'teacher',
                 ],
                 'constraints' => [
                     new NotBlank([
-                        'message' => 'Veuillez choisir un profil (étudiant ou professeur).',
+                        'message' => 'Veuillez choisir un profil.',
                     ]),
                 ],
             ])
-            ->add('agreeTerms', CheckboxType::class, [
-                'mapped' => false,
-                'constraints' => [
-                    new IsTrue([
-                        'message' => 'You should agree to our terms.',
-                    ]),
+
+            ->add('email', EmailType::class, [
+                'label' => 'Email',
+                'attr' => [
+                    'placeholder' => 'exemple@edulearn.fr',
                 ],
-            ])
-            ->add('plainPassword', PasswordType::class, [
-                'mapped' => false,
-                'attr' => ['autocomplete' => 'new-password'],
                 'constraints' => [
                     new NotBlank([
-                        'message' => 'Please enter a password',
+                        'message' => 'Veuillez saisir une adresse email.',
+                    ]),
+                ],
+            ])
+
+            ->add('name', TextType::class, [
+                'label' => 'Prénom',
+                'attr' => [
+                    'placeholder' => 'Jean',
+                ],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez saisir votre prénom.',
+                    ]),
+                ],
+            ])
+
+            ->add('surname', TextType::class, [
+                'label' => 'Nom',
+                'attr' => [
+                    'placeholder' => 'Dupont',
+                ],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez saisir votre nom.',
+                    ]),
+                ],
+            ])
+
+            ->add('plainPassword', PasswordType::class, [
+                'mapped' => false,
+                'label' => 'Mot de passe',
+                'attr' => [
+                    'autocomplete' => 'new-password',
+                    'placeholder' => '••••••••',
+                ],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez saisir un mot de passe.',
                     ]),
                     new Length([
                         'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
+                        'minMessage' => 'Le mot de passe doit contenir au moins {{ limit }} caractères.',
                         'max' => 4096,
+                    ]),
+                ],
+            ])
+
+            ->add('agreeTerms', CheckboxType::class, [
+                'mapped' => false,
+                'label' => 'J’accepte les conditions d’utilisation',
+                'constraints' => [
+                    new IsTrue([
+                        'message' => 'Vous devez accepter les conditions d’utilisation.',
                     ]),
                 ],
             ]);
     }
 
-    /**
-     * Configure les options du formulaire.
-     *
-     * Associe le formulaire à l'entité User.
-     *
-     * @param OptionsResolver $resolver le gestionnaire d'options
-     */
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
